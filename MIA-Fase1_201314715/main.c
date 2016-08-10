7226d669be2a3a2499190f95b0e044e74dd96855
mmutz2015 <mmutzusac@gmail.com>
2016-08-10 09:58:01 -0600

    comienzo de la funcion fdisk incluyendo su parte del analizador completo, a espera de realizar las respectivas evaluaciones

--- MIA-Fase1_201314715/main.c
+++ MIA-Fase1_201314715/main.c
@@ -37,6 +37,7 @@ void CreateFolder(char * path);
 int analizar(char * cad, int n);
 void insert_mbr(char *fname, int size);
 int elimDisco(char * cadena);
+int fDisk(char * cadena);
 /****ANALIZADOR******/
 int analizar(char * cad, int n){
 int estado  = 0;
@@ -85,6 +86,8 @@ spliteo = strtok(cad, " ");
            estado = 1;
        }else if(strcasecmp("rmdisk", spliteo)==0){
            estado = 2;
+       }else if(strcasecmp("fdisk", spliteo)==0){
+           estado = 3;
        }
        else{
            //errores de escritura
@@ -104,6 +107,10 @@ spliteo = strtok(cad, " ");
        case 2:
            printf("comando rmdisk\n");
            elimDisco(entrada);
+       case 3:
+           printf("comando fdisk\n");
+           fDisk(entrada);
+           break;
        default:
           //error
            printf("error comando no valido\n");
@@ -113,6 +120,188 @@ spliteo = strtok(cad, " ");
        return 1;
 
 }
+
+/***********CREACION DE PARTICIONES**********/
+int fDisk(char * cadena){
+    int tam;
+    int size;
+    char unit='\0';
+    char path[100];
+    char type = '\0';
+    char deldesic[10];
+    char fit[5];
+    char name[100];
+    int add;
+    int estado = -1;
+    path[0] = '\0';
+    deldesic[0] = '\0';
+    fit[0]='\0';
+    name[0] = '\0';
+char * spliteo;
+    spliteo = strtok(cadena, " ");
+
+    while(spliteo!= NULL){
+
+        if(strcasecmp("fdisk", spliteo) ==0){
+                   estado = 0;
+          }else if(strcasecmp("-size::", spliteo)==0){
+            estado = 1;
+
+        }else if(strcasecmp("+unit::", spliteo)==0){
+            estado = 2;
+        }else if(strcasecmp("-path::", spliteo)){
+            estado = 3;
+        }else if(strcasecmp("+type::", spliteo)==0){
+            estado = 4;
+        }else if(strcasecmp("+fit::", spliteo)==0){
+            estado = 5;
+        }else if(strcasecmp("+delete::", spliteo)==0){
+            estado = 6;
+        }else if(strcasecmp("-name::", spliteo)==0){
+            estado = 7;
+        }else if(strcasecmp("+add::", spliteo)==0){
+            estado = 8;
+        }
+
+        switch (estado) {
+        case 0:
+           //no hace nada desecha;
+            break;
+        case 1:
+            estado = 9;
+            break;
+        case 2:
+            estado = 10;
+            break;
+        case 3:
+            estado = 11;
+            break;
+        case 4:
+            estado = 12;
+            break;
+        case 5:
+            estado = 13;
+            break;
+        case 6:
+            estado = 14;
+            break;
+        case 7:
+            estado = 15;
+            break;
+        case 8:
+            estado = 16;
+            break;
+        case 9:
+            size = atoi(spliteo);
+            if(size <0){
+                printf("el tamanio de la particion debe de ser mayor a 2 MB\n");
+            }
+            estado = 0;
+            break;
+        case 10:
+            if(strcasecmp("K",spliteo)==0){
+                //crear e kilos
+                unit = 'k';
+
+            }else if(strcasecmp("M",spliteo)==0){
+                //crear en megas
+                unit  = 'm';
+               } else if(strcasecmp("B",spliteo)==0){
+                                //crear en megas
+                                unit  = 'b';
+            }else{
+                //viene un caracter erroneo
+                printf("caracter de unidades invalido\n");
+
+            }
+            estado = 0;
+            break;
+        case 11:
+
+            for(tam =0; spliteo[tam]!='\0';tam++){
+
+            }
+            if(spliteo[tam-5]=='.'&& spliteo[tam-4]=='d' && spliteo[tam-3]=='s'&& spliteo[tam-2]=='k'){
+
+                    strcpy(path,spliteo);
+
+                }else{
+                    printf("el nombre del disco no posee extension\n");
+                }
+             estado = 0;
+            break;
+        case 12:
+            if(strcasecmp("P",spliteo)==0){
+                //crear e kilos
+                type = 'P';
+
+            }else if(strcasecmp("E",spliteo)==0){
+                //crear en megas
+                type  = 'E';
+           } else if(strcasecmp("L",spliteo)==0){
+                //crear en megas
+                type  = 'L';
+            }else{
+                //viene un caracter erroneo
+                printf("caracter de particion invalido\n");
+
+            }
+            break;
+        case 13:
+            if(strcasecmp("BF",spliteo)==0){
+                //crear e kilos
+                strcpy(fit,spliteo);
+
+
+            }else if(strcasecmp("FF",spliteo)==0){
+                //crear en megas
+                strcpy(fit,spliteo);
+
+               } else if(strcasecmp("WF",spliteo)==0){
+                                //crear en megas
+                strcpy(fit,spliteo);
+
+            }else{
+                //viene un caracter erroneo
+                printf("caracteres de fit invalidos\n");
+
+            }
+            break;
+        case 14:
+            if(strcasecmp("Fast",spliteo)==0){
+                //crear e kilos
+                strcpy(deldesic,spliteo);
+
+
+            }else if(strcasecmp("Full",spliteo)==0){
+                //crear en megas
+                strcpy(deldesic,spliteo);
+
+            }else{
+                //viene un caracter erroneo
+                printf("caracteres de delete invalidos\n");
+
+            }
+
+            break;
+        case 15:
+            strcpy(path,spliteo);
+
+             estado = 0;
+            break;
+        case 16:
+            add = atoi(spliteo);
+            break;
+        default:
+            break;
+        }
+
+        spliteo = strtok(NULL, " \n");
+    }
+return 1;
+}
+
+
 /******ELIMINAR DISCO**********/
 
 int elimDisco(char * cadena){
